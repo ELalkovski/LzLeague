768951296fb8f4075b3e243cde9a7abfe9f3ace7
emil.lalkovski <emo_55@abv.bg>
2021-01-24 15:25:01 +0200

    Resolve merge conflicts

--- LzLeague.Services/Admin/TeamsService.cs
+++ LzLeague.Services/Admin/TeamsService.cs
@@ -240,12 +240,8 @@
                 .Teams
                 .Include(t => t.Group)
                 .ThenInclude(g => g.Teams)
-                .Include(t => t.PlayedMatches)
-<<<<<<< .mine
+                .Include(t => t.HomePlayedMatches)
                 .Include(t => t.AwayPlayedMatches)
-=======
-
->>>>>>> .theirs
                 .FirstOrDefaultAsync(t => t.Id == teamId);
         }
 
